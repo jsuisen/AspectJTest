86df6fb189db9cede6af52ae997ebfbccf5db2b6
xhq <xhq@allfully.com>
2020-02-27 16:36:59 +0800

    添加handler 捕获

--- app/src/main/java/com/gw/aspectjtest/MainActivity.java
+++ app/src/main/java/com/gw/aspectjtest/MainActivity.java
@@ -6,12 +6,19 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         boolean flag = AOPLoginUtils.Login("张三","123456");
+        try{
+            throw new IOException();
+        }catch (IOException ex){
+            Log.i("xhq","throw exception");
+        }
         Log.i("OOP","test-"+flag);
     }
 
